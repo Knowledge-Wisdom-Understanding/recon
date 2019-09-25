1c09efc944ece618a07e145c0a74d161542c49d7
Knowledge-Wisdom-Understanding <philmiller1992@gmail.com>
2019-09-25 03:53:07 -0400

    upgraded searchsploit.py to yaml parser

--- lib/searchsploits.py
+++ lib/searchsploits.py
@@ -5,7 +5,7 @@ from sty import fg, bg, ef, rs
 from lib import nmapParser
 from lib import brute
 from subprocess import call
-from utils import config_paths
+from utils import config_parser
 
 
 class Search:
@@ -34,48 +34,41 @@ class Search:
         products = np.all_products
         http_title = np.http_script_title
         ignore = ["apache", "mysql"]
-        c = config_paths.Configurator(self.target)
-        c.createConfig()
+        c = config_parser.CommandParser(f"{os.getcwd()}/config/config.yaml", self.target)
         ### FTP searchsploit product ###
         if len(ftp_product) == 1:
-            if not os.path.exists(f"""{c.getPath("vulnDir")}"""):
-                os.makedirs(f"""{c.getPath("vulnDir")}""")
+            if not os.path.exists(c.getPath("vuln", "vulnDir")):
+                os.makedirs(c.getPath("vuln", "vulnDir"))
             string_ftp = " ".join(map(str, ftp_product))
             lowercase_string_ftp = str(string_ftp).lower()
-            ftp_cmd = (
-                f"""searchsploit {lowercase_string_ftp} > {c.getPath("ftp_vulns")}"""
-            )
+            ftp_cmd = c.getCmd("vuln", "searchsploit", strang=lowercase_string_ftp, name="ftp")
             print(cmd_info, ftp_cmd)
             call(ftp_cmd, shell=True)
 
         #### SSH searchsploit product ###
         if len(ssh_product) == 1:
-            if not os.path.exists(f"""{c.getPath("vulnDir")}"""):
-                os.makedirs(f"""{c.getPath("vulnDir")}""")
+            if not os.path.exists(c.getPath("vuln", "vulnDir")):
+                os.makedirs(c.getPath("vuln", "vulnDir"))
             string_ssh = " ".join(map(str, ssh_product))
             lowercase_string_ssh = str(string_ssh).lower()
-            ssh_cmd = (
-                f"""searchsploit {lowercase_string_ssh} > {c.getPath("ssh_vulns")}"""
-            )
+            ssh_cmd = c.getCmd("vuln", "searchsploit", strang=lowercase_string_ssh, name="ssh")
             print(cmd_info, ssh_cmd)
             call(ssh_cmd, shell=True)
 
         #### SMTP searchsploit product ###
         if len(smtp_product) == 1:
-            if not os.path.exists(f"""{c.getPath("vulnDir")}"""):
-                os.makedirs(f"""{c.getPath("vulnDir")}""")
+            if not os.path.exists(c.getPath("vuln", "vulnDir")):
+                os.makedirs(c.getPath("vuln", "vulnDir"))
             string_smtp = " ".join(map(str, smtp_product))
             lowercase_string_smtp = str(string_smtp).lower()
-            smtp_cmd = (
-                f"""searchsploit {lowercase_string_smtp} > {c.getPath("smtp_vulns")}"""
-            )
+            smtp_cmd = c.getCmd("vuln", "searchsploit", strang=lowercase_string_smtp, name="smtp")
             print(cmd_info, smtp_cmd)
             call(smtp_cmd, shell=True)
 
         #### HTTP Title searchsploit (hoping for CMS in title) ##########
         if len(http_title) >= 1:
-            if not os.path.exists(f"""{c.getPath("vulnDir")}"""):
-                os.makedirs(f"""{c.getPath("vulnDir")}""")
+            if not os.path.exists(c.getPath("vuln", "vulnDir")):
+                os.makedirs(c.getPath("vuln", "vulnDir"))
             if len(http_title) > 1:
                 for title in http_title:
                     string_title = " ".join(map(str, title))
@@ -90,8 +83,8 @@ class Search:
                         pass
                     else:
                         first_word = lowercase_title.split(" ", 1)[0]
-                        http_cmd = f"""searchsploit {lowercase_title} >> {c.getPath("http_vulns")}"""
-                        http_cmd2 = f"""searchsploit {first_word} >> {c.getPath("http_vulns")}"""
+                        http_cmd = c.getCmd("vuln", "searchsploit", strang=lowercase_title, name="http-title")
+                        http_cmd2 = c.getCmd("vuln", "searchsploit", strang=first_word, name=f"{first_word}")
                         print(cmd_info, http_cmd)
                         print(cmd_info, http_cmd2)
                         call(http_cmd, shell=True)
@@ -110,10 +103,8 @@ class Search:
                     pass
                 else:
                     first_word = lowercase_title.split(" ", 1)[0]
-                    http_cmd = f"""searchsploit "{lowercase_title}" >> {c.getPath("http_vulns")}"""
-                    http_cmd2 = (
-                        f"""searchsploit "{first_word}" >> {c.getPath("http_vulns")}"""
-                    )
+                    http_cmd = c.getCmd("vuln", "searchsploit", strang=lowercase_title, name="http-title")
+                    http_cmd2 = c.getCmd("vuln", "searchsploit", strang=first_word, name=f"{first_word}")
                     print(cmd_info, http_cmd)
                     print(cmd_info, http_cmd2)
                     call(http_cmd, shell=True)
@@ -132,14 +123,11 @@ class Search:
                     if fw in ignore:
                         pass
                     else:
-                        product_cmd = f"""echo "{cmd_info} {lowercase_product}" >> {c.getPath("all_vulns")}"""
-                        product_cmd2 = f"""searchsploit "{lowercase_product}" >> {c.getPath("all_vulns")}"""
-                        product_cmd3 = (
-                            f"""echo "{cmd_info} {fw}" >> {c.getPath("all_vulns")}"""
-                        )
-                        product_cmd4 = (
-                            f"""searchsploit "{fw}" >> {c.getPath("all_vulns")}"""
-                        )
+                        product_cmd = f"""echo "{cmd_info} {lowercase_product}" >> {c.getPath("vuln","all_vulns")}"""
+                        product_cmd2 = c.getCmd("vuln", "searchsploit", strang=lowercase_product, name="all-services")
+                        product_cmd3 = f"""echo "{cmd_info} {fw}" >> {c.getPath("vuln","all_vulns")}"""
+                        product_cmd4 = f"""searchsploit "{fw}" >> {c.getPath("vuln","all_vulns")}"""
+                        http_cmd2 = c.getCmd("vuln", "searchsploit", strang=fw, name=f"all-services")
                         print(cmd_info, product_cmd2)
                         call(product_cmd, shell=True)
                         call(product_cmd2, shell=True)
@@ -162,11 +150,10 @@ class Search:
         np.openPorts()
         ssh_product = np.ssh_product
         ssh_version = np.ssh_version
-        c = config_paths.Configurator(self.target)
-        c.createConfig()
-        ## Check what version OPENSSH is
-        ## If OpenSSH version is less than 7.7, Enumerate Users
-        ## If valid Unique User is found, Brute Force Passwords
+        c = config_parser.CommandParser(f"{os.getcwd()}/config/config.yaml", self.target)
+        # Check what version OPENSSH is
+        # If OpenSSH version is less than 7.7, Enumerate Users
+        # If valid Unique User is found, Brute Force Passwords
         if len(ssh_product) == 1:
             string_ssh_version = " ".join(map(str, ssh_version))
             lowercase_ssh_version = str(string_ssh_version).lower()
@@ -174,17 +161,11 @@ class Search:
             int_first_two_nums = float(first_two_nums)
             if ssh_product[0] == "OpenSSH":
                 if int_first_two_nums < float(7.7):
-                    # ssh_port = np.ssh_ports[0]
-                    print(
-                        f"""{cmd_info} {blue}{ssh_product[0]} {ssh_version[0]}{reset} is {red}vulnerable to username Enumeration{reset}"""
-                    )
+                    ssh_port = np.ssh_ports
+                    print(f"""{cmd_info} {blue}{ssh_product[0]} {ssh_version[0]}{reset} is {red}vulnerable to username Enumeration{reset}""")
                     print(f"""{green}Consider running:{reset}""")
-                    print(
-                        f"""{manual_cmd_info} python {c.getPath("ssh_user_enum")} --port 22 --userList {c.getPath("usernames_wordlist")} {self.target} --outputFile {c.getPath("ssh_usernames")} --outputFormat json"""
-                    )
+                    print(f"""{manual_cmd_info} {c.getCmd("ssh", "ssh_user_enum", port=ssh_port[0])}""")
                     # sb = brute.Brute(self.target, "ssh", ssh_port)
                     # sb.SshUsersBrute()
                 else:
-                    print(
-                        f"""{cmd_info} {blue}{ssh_product[0]} {ssh_version[0]}{reset} is {red}NOT{reset} vulnerable to username Enumeration"""
-                    )
+                    print(f"""{cmd_info} {blue}{ssh_product[0]} {ssh_version[0]}{reset} is {red}NOT{reset} vulnerable to username Enumeration""")
