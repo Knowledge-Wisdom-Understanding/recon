ec4532f9eb11ac8030dc43567c658e86bf02e6ef
Knowledge-Wisdom-Understanding <philmiller1992@gmail.com>
2019-09-30 13:19:29 -0400

    :heart: bleed banner

--- utils/peaceout_banner.py
+++ utils/peaceout_banner.py
@@ -85,3 +85,50 @@ _/oo OOOOO oo`  ooo   ooo  'o!o!o                  o!o!o` 'o!o         o!o`
         color = random_color(self)
         print_art(self, msg, color)
         self.banner = peace
+
+
+class heartbleed:
+    def __init__(self, target):
+        self.target = target
+        self.blood_banner = """"""
+
+    def bleedOut(self):
+        """Select a random color from valid colors and return a random color
+        to colorize the Banner."""
+
+        bleed = rf"""
+          |  \ \ | |/ /
+          |  |\ `' ' /
+          |  ;'aorta \      / , pulmonary
+          | ;    _,   |    / / ,  arteries
+ superior | |   (  `-.;_,-' '-' ,
+vena cava | `,   `-._       _,-'_
+          |,-`.    `.)    ,<_,-'_, pulmonary
+         ,'    `.   /   ,'  `;-' _,  veins
+        ;        `./   /`,    \-'
+        | right   /   |  ;\   |\
+        | atrium ;_,._|_,  `, ' \      √v^√v^♥√v^√v^√
+        |        \    \ `       `,
+        `      __ `    \   left  ;,
+         \   ,'  `      \,  ventricle
+          \_(            ;,      ;;
+          |  \           `;,     ;;
+ inferior |  |`.          `;;,   ;'
+vena cava |  |  `-.        ;;;;,;' FL
+          |  |    |`-.._  ,;;;;;'
+          |  |    |   | ``';;;' {self.target} is vulnerable to heartbleed!!
+          |__|   aorta                 ______.   .___                   ____.
+          |  |__   ____ _____ ________/  |\_ |__ |  |   ____   ____   __| _/
+          |  |  \_/ __ \\__  \\_  __ \   __\ __ \|  | _/ __ \_/ __ \ / __ | 
+          |   Y  \  ___/ / __ \|  | \/|  | | \_\ \  |_\  ___/\  ___// /_/ | 
+          |___|  /\___  >____  /__|   |__| |___  /____/\___  >\___  >____ | 
+              \/     \/     \/                \/          \/     \/     \/ 
+                        """
+
+        def print_art(self, msg):
+            colored_art = colored(msg, "red")
+            print(colored_art)
+
+        msg = bleed
+        print_art(self, msg)
+        self.blood_banner = bleed
