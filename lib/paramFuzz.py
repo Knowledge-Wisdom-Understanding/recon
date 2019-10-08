8638d11fd315ccf63f6ffee2732b541913d73ebe
Knowledge-Wisdom-Understanding <philmiller1992@gmail.com>
2019-10-08 08:58:27 -0400

    Added PHP Param Auto Fuzz Option --FUZZ

--- lib/paramFuzz.py
+++ lib/paramFuzz.py
@@ -7,19 +7,64 @@ from utils import helper_lists
 from urllib.parse import urlsplit
 from sty import fg, rs
 import requests
+from urllib3.exceptions import InsecureRequestWarning
+import warnings
+import contextlib
+import logging
 
 
 class ParamFuzzer:
-    """This Class, DirsearchURLS is reponsible for sorting all the found URL's
-    from Dirsearches report output and then it will combined them in to one unique
-    list that will be fed to Aquatone to generate a nice HTML report that will
-    Be opened up in the firefox web browser."""
+    """This Class, Param Fuzzer will parse all found urls from dirsearch's output
+    to only include urls ending in .php. Next, ParamFuzzer will call parameth and
+    begin fuzzing for valid .php?parameters and log output to a file."""
 
     def __init__(self, target):
         self.target = target
         self.processes = []
 
+    def loginator(self, executed_command):
+        c = config_parser.CommandParser(f"{os.getcwd()}/config/config.yaml", self.target)
+        logging.basicConfig(
+            filename=c.getPath("report", "log"),
+            format='%(asctime)s %(message)s',
+            datefmt='%m/%d/%Y %I:%M:%S %p',
+            level=logging.INFO
+        )
+        logging.info(f"[+] {executed_command}")
+
+    @contextlib.contextmanager
+    def no_ssl_verification(self):
+        old_merge_environment_settings = requests.Session.merge_environment_settings
+        opened_adapters = set()
+
+        def merge_environment_settings(self, url, proxies, stream, verify, cert):
+            # Verification happens only once per connection so we need to close
+            # all the opened adapters once we're done. Otherwise, the effects of
+            # verify=False persist beyond the end of this context manager.
+            opened_adapters.add(self.get_adapter(url))
+
+            settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
+            settings['verify'] = False
+
+            return settings
+
+        requests.Session.merge_environment_settings = merge_environment_settings
+
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', InsecureRequestWarning)
+                yield
+        finally:
+            requests.Session.merge_environment_settings = old_merge_environment_settings
+
+            for adapter in opened_adapters:
+                try:
+                    adapter.close()
+                except:
+                    pass
+
     def fuzzMaster(self):
+        """fuzzMaster will run parameth to fuzz for valid .php parameters. Will add more extensions soon."""
         c = config_parser.CommandParser(f"{os.getcwd()}/config/config.yaml", self.target)
         hl = helper_lists.ignoreURLS()
         ignore_urls = hl.ignore_urls
@@ -48,18 +93,31 @@ class ParamFuzzer:
                     pass
                 if len(php_urls) != 0 and (len(php_urls) < 10):
                     for url in php_urls:
-                        session = requests.Session()
-                        res = session.get(url)
-                        cookie_dict.update(session.cookies.get_dict())
-                        output_name = urlsplit(url).path
-                        upath = str(output_name).replace("/", "-")
-                        if not cookie_dict:
-                            fuzz_cmds.append(c.getCmd("web", "parameth", url=url, upath=upath))
-                        else:
-                            cookie_string = " ".join("{}={}".format(*i) for i in cookie_dict.items())
-                            fuzz_cmds.append(c.getCmd("web", "paramethCookie", url=url, cookies=cookie_string, upath=upath))
+                        with self.no_ssl_verification():
+                            try:
+                                session = requests.Session()
+                                res = session.get(url)
+                                cookie_dict.update(session.cookies.get_dict())
+                                output_name = urlsplit(url).path
+                                upath = str(output_name).replace("/", "-")
+                                if not cookie_dict:
+                                    fuzz_cmds.append(c.getCmd("web", "parameth", url=url, upath=upath))
+                                else:
+                                    cookie_string = " ".join("{}={}".format(*i) for i in cookie_dict.items())
+                                    fuzz_cmds.append(c.getCmd("web", "paramethCookie", url=url, cookies=cookie_string, upath=upath))
+                            except requests.exceptions.ConnectionError as ce_error:
+                                print("Connection Error: ", ce_error)
+                                break
+                            except requests.exceptions.Timeout as t_error:
+                                print("Connection Timeout Error: ", t_error)
+                                break
+                            except requests.exceptions.RequestException as re:
+                                print("Some Ambiguous Exception:", re)
+                                break
                 if len(fuzz_cmds) != 0:
+                    print(f"{fg.li_cyan}Fuzzing .php Params ! {fg.rs}")
                     for i in fuzz_cmds:
-                        print(i)
+                        print(f"[{fg.li_green}+{fg.rs}] {i}")
+                        self.loginator(i)
                         call(i, shell=True)
                 # self.processes = tuple(fuzz_cmds)
