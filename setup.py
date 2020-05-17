ef1510ececc2d6d17d82e8ffd5e12f20d43db243
root <root@kali.wildstyle9.net>
2020-05-16 20:24:16 -0400

    fixed bugs

--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(name='autorecon',
       license='MIT',
       packages=['autorecon', 'autorecon.lib', 'autorecon.utils'],
       include_package_data=True,
-      install_requires=['sty', 'tqdm', 'PyYAML', 'wfuzz', 'requests', 'python_hosts', 'psutil', 'python_libnmap', 'beautifulsoup4', 'termcolor', 'xmltodict', 'impacket'],
+      install_requires=['sty', 'tqdm', 'PyYAML', 'wfuzz', 'requests', 'python_hosts', 'psutil', 'python_libnmap',
+                        'beautifulsoup4', 'termcolor', 'xmltodict','pyasn1' 'impacket', 'shodan','python-ldap','pycurl'],
       entry_points={'console_scripts': ['autorecon=autorecon.__main__:main']})
