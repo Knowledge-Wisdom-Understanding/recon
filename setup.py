9a8c9930af92a24aa3f6babe251cb10ef368b0bf
Knowledge-Wisdom-Understanding <root@kali.wildstyle9.net>
2020-12-22 23:17:51 -0500

    fixed typo in setup.py added missing comma

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(name='autorecon',
       packages=['autorecon', 'autorecon.lib', 'autorecon.utils'],
       include_package_data=True,
       install_requires=['sty', 'tqdm', 'PyYAML', 'wfuzz', 'requests', 'python_hosts', 'psutil', 'python_libnmap',
-                        'beautifulsoup4', 'termcolor', 'xmltodict', 'pyasn1', 'python-ldap', 'ldap3', 'impacket', 'shodan', 'pycurl', 'tabulate', 'urllib3' 'loguru'],
+                        'beautifulsoup4', 'termcolor', 'xmltodict', 'pyasn1', 'python-ldap', 'ldap3', 'impacket',
+                        'shodan', 'pycurl', 'tabulate', 'urllib3', 'loguru'],
       entry_points={'console_scripts': ['autorecon=autorecon.__main__:main']})
