018bcd5f09ca4eb6b463c571ace3f72c06463ae8
Knowledge-Wisdom-Understanding <root@kali.wildstyle9.net>
2021-01-02 02:29:22 -0500

    refactoring code

--- autorecon/lib/aqua.py
+++ autorecon/lib/aqua.py
@@ -3,7 +3,7 @@
 import os
 from sty import fg
 from autorecon.lib import nmapParser
-from subprocess import call, check_output, STDOUT
+from subprocess import call
 from shutil import which
 from autorecon.utils import config_parser
 
@@ -43,11 +43,16 @@ class Aquatone:
         all_web_proxy_ports_comma_list = ",".join(map(str, all_web_proxy_ports))
         if not os.path.exists(c.getPath("web", "aquatoneDir")):
             os.makedirs(c.getPath("web", "aquatoneDir"))
-        if os.path.exists(c.getPath("web", "aquatoneDirUrls")):
-            check_lines = f"""wc -l {c.getPath("web","aquatoneDirUrls")} | cut -d ' ' -f 1"""
-            num_urls = check_output(check_lines, stderr=STDOUT, shell=True).rstrip()
-            # ToDo: open urls.txt and sort urls by occurance of response codes.
-            if int(num_urls) < 150 and (int(num_urls) != 0):
+        aquatone_urls = c.getPath("web", "aquatoneDirUrls")
+
+        def get_num_urls(filepath: str) -> int:
+            with open(filepath, 'r') as fp:
+                _num_urls = len([l.rstrip() for l in fp.readlines() if l.startswith('http')])
+            return _num_urls
+
+        if os.path.exists(aquatone_urls):
+            num_urls = get_num_urls(aquatone_urls)
+            if num_urls < 150 and (num_urls != 0):
                 aquatone_cmd = c.getCmd("web", "aquatone", allWebPorts=all_web_ports_comma_list)
                 print(cmd_info, aquatone_cmd)
                 call(aquatone_cmd, shell=True)
@@ -58,10 +63,10 @@ class Aquatone:
                         print(f"""{fg.cyan}Opening Aquatone Report {fg.rs}""")
                         open_in_ff_cmd = f"""firefox {c.getPath("web","aquatoneReport")} &"""
                         call(open_in_ff_cmd, shell=True)
-        if os.path.exists(c.getPath("proxy", "aquatoneDirProxyUrls")):
-            check_lines = f"""wc -l {c.getPath("proxy","aquatoneDirProxyUrls")} | cut -d ' ' -f 1"""
-            num_urls = check_output(check_lines, stderr=STDOUT, shell=True).rstrip()
-            if int(num_urls) < 150 and (int(num_urls) != 0):
+        aquatone_proxy_urls = c.getPath("proxy", "aquatoneDirProxyUrls")
+        if os.path.exists(aquatone_proxy_urls):
+            num_urls = get_num_urls(aquatone_proxy_urls)
+            if num_urls < 150 and (num_urls != 0):
                 aquatone_cmd = c.getCmd("proxy", "aquatoneProxy", allWebProxyPorts=all_web_proxy_ports_comma_list, proxyPorts=proxy_ports[0])
                 print(cmd_info, aquatone_cmd)
                 call(aquatone_cmd, shell=True)
